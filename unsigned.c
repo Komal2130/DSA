482b558c2e75102d0f639352e3676aaf8e1b0af7
Komal2130 <114568361+Komal2130@users.noreply.github.com>
2024-04-29 15:36:09 +0530

    Add files via upload

---
+++ unsigned.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    // unsigned char a = 1;
+    // unsigned short int a = 1;
+    unsigned long int a = 1;
+    while(a)
+    {
+        printf("%d \t", a);
+        a++;
+    }
+}
