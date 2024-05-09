76f148f331e8947504973f98dcb83377a1928cd5
Komal2130 <komalchoudhary.cse25@jecrc.ac.in>
2024-05-09 10:58:45 +0530

    Polymorphism

---
+++ OOP/Polymorphism/overloading.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Test
+{
+    public:
+    void show()
+    {
+        cout<<"Hello";
+    }
+    void show(int x)
+    {
+        cout<<x;
+    }
+    void show(char x)
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    Test t;
+    t.show('A');
+    t.show();
+    t.show(4);
+}
